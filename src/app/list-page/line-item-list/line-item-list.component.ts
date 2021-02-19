39b37cfe812d774312f8421a029c7db757a14d75
Doug Brown <dougb@finitemonkey.com>
2021-02-19 10:02:42 -0600

    Code formatting

--- src/app/list-page/line-item-list/line-item-list.component.ts
+++ src/app/list-page/line-item-list/line-item-list.component.ts
@@ -5,24 +5,56 @@ import { LineItemListHeaderModel } from './line-item-list-header-model';
 @Component({
   selector: 'app-line-item-list',
   templateUrl: './line-item-list.component.html',
-  styleUrls: ['./line-item-list.component.scss']
+  styleUrls: ['./line-item-list.component.scss'],
 })
 export class LineItemListComponent implements OnInit {
   header: LineItemListHeaderModel = {
-    id: "ID", client: "Client", status: "Status", date: "Date", amount: "Amount"
-  }
-  
+    id: 'ID',
+    client: 'Client',
+    status: 'Status',
+    date: 'Date',
+    amount: 'Amount',
+  };
+
   items: LineItemModel[] = [
-    {'id': 1, 'client': 'ABC Client', 'status': 'Approved', 'date': new Date(), 'amount': (1000 * Math.random()).toFixed(2) },
-    {'id': 2, 'client': '123 Client', 'status': 'Approved', 'date': new Date(), 'amount': (1000 * Math.random()).toFixed(2) },
-    {'id': 3, 'client': 'DEF Client', 'status': 'Declined', 'date': new Date(), 'amount': (1000 * Math.random()).toFixed(2) },
-    {'id': 4, 'client': '456 Client', 'status': 'Approved', 'date': new Date(), 'amount': (1000 * Math.random()).toFixed(2) },
-    {'id': 5, 'client': 'GHI Client', 'status': 'Mystery', 'date': new Date(), 'amount': (1000 * Math.random()).toFixed(2) },
+    {
+      id: 1,
+      client: 'ABC Client',
+      status: 'Approved',
+      date: new Date(),
+      amount: (1000 * Math.random()).toFixed(2),
+    },
+    {
+      id: 2,
+      client: '123 Client',
+      status: 'Approved',
+      date: new Date(),
+      amount: (1000 * Math.random()).toFixed(2),
+    },
+    {
+      id: 3,
+      client: 'DEF Client',
+      status: 'Declined',
+      date: new Date(),
+      amount: (1000 * Math.random()).toFixed(2),
+    },
+    {
+      id: 4,
+      client: '456 Client',
+      status: 'Approved',
+      date: new Date(),
+      amount: (1000 * Math.random()).toFixed(2),
+    },
+    {
+      id: 5,
+      client: 'GHI Client',
+      status: 'Mystery',
+      date: new Date(),
+      amount: (1000 * Math.random()).toFixed(2),
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
